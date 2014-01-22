a0bff7c1507f73bb13c120ffad59528ca82c1c09
syatyongithub <syatyon+github@gmail.com>
2014-01-22 23:43:16 +0900

    ノードをトラバースしてダンプ出力する機能を実装して、ノードの付け外し機能を確認。

--- src/TDacTest.cpp
+++ src/TDacTest.cpp
@@ -10,19 +10,52 @@
 using namespace std;
 
 #include <TreeList.h>
+#include <stdio.h>
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+#ifndef NULL
+#	define NULL 0
+#endif
 
-	TreeList root;
-	TreeList sub0;
-	TreeList sub0_0;
-	TreeList sub0_1;
+class NameTreeList : public TreeList
+{
+public:
+	NameTreeList(const char * const name) :
+		mName(name)
+	{
 
-	TreeList sub1;
-	TreeList sub1_0;
-	TreeList sub1_1;
-	TreeList sub1_2;
+	}
+
+	void DumpPrint(int depth = 0)
+	{
+		for(int i=0; i<depth; i++)
+		{
+			printf(" ");
+		}
+		printf("Node : %s\n", mName);
+	
+		std::list<TreeList*>::iterator it;
+		for(it = GetChild()->begin(); it != GetChild()->end(); it++)
+		{
+			dynamic_cast<NameTreeList*>(*it)->DumpPrint(depth+1);
+		}
+	}
+
+private:
+	const char * const mName;
+};
+
+
+int main(int argc, char *argv[]) 
+{
+	NameTreeList root("root");
+	NameTreeList sub0("sub0");
+	NameTreeList sub0_0("sub0_0");
+	NameTreeList sub0_1("sub0_1");
+
+	NameTreeList sub1("sub1");
+	NameTreeList sub1_0("sub1_0");
+	NameTreeList sub1_1("sub1_1");
+	NameTreeList sub1_2("sub1_2");
 
 	sub0.AddChild(&sub0_0);
 	sub0.AddChild(&sub0_1);
@@ -35,6 +68,19 @@ int main() {
 	root.AddChild(&sub1);
 
 	root.DumpPrint();
+	printf("\n");
+
+	sub0_0.AddChild(&sub0_1);
+	sub1_0.AddChild(&sub1_1);
+	sub1_1.AddChild(&sub1_2);
+
+	root.DumpPrint();
+	printf("\n");
+
+	sub1_2.AddChild(&sub0);
+
+	root.DumpPrint();
+	printf("\n");
 
 	return 0;
 }
